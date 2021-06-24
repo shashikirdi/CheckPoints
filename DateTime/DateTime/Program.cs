88d524c84a1db296b503b2c546ea558735fa7fe0
shashikirdi <shashikirdi@gmail.com>
2021-06-25 00:17:47 +0200

    mini project - level 1

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DateTime dt = new DateTime();
+            dt = DateTime.Now;
+            input = dt.Console.ReadLine();
+        }
+    }
+}
